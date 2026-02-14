7eed8f8629f8d7e34b9ba3555d833b9e4b25a625
rodriguezmerelin56-boop <rodriguezmerelin56@gmail.com>
2026-02-13 21:47:37 -0600

    SUBIENDO SEGUNDO PROYECTO

---
+++ formulario/formulario/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(735, 25);
+            label1.TabIndex = 0;
+            label1.Text = "¡BIENVENIDO AL FORMULARIO DE MERELIN RODIGUEZ MI MATRICULA ES LR-2024-02534";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            ForeColor = SystemColors.InfoText;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
